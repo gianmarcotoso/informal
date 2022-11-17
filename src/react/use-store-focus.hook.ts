3f8ecd74926682d55b883962d19e26729a961284
Gian Marco Toso <info@gianmarcotoso.com>
2022-11-17 11:39:14 +0100

    useRef instead of useMemo in useStoreFocus

--- src/react/use-store-focus.hook.ts
+++ src/react/use-store-focus.hook.ts
@@ -1,9 +1,9 @@
-import { useMemo, useSyncExternalStore } from 'react'
+import { useRef, useSyncExternalStore } from 'react'
 import { createFocus } from '../create-focus'
 import { Store, PathElement, Setter } from '../types'
 
 export function useStoreFocus<T, K>(store: Store<T>, ...path: PathElement[]): [K, Setter<K>, Store<K>] {
-	const focus = useMemo(() => createFocus<T, K>(store, ...path), [store, path])
+	const { current: focus } = useRef(createFocus<T, K>(store, ...path))
 	const data = useSyncExternalStore(focus.subscribe, focus.getData, focus.getData)
 
 	return [data, focus.setData, focus]
