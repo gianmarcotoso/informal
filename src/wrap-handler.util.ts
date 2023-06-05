aedd80aa877b9f60a5d3e82110096dfc88edcf36
Gian Marco Toso <gianmarco.toso@gmail.com>
2023-06-05 09:27:25 +0200

    make wrap-handler more flexible (ts)

--- src/wrap-handler.util.ts
+++ src/wrap-handler.util.ts
@@ -1,12 +1,24 @@
 import { normalizePath } from './normalize-path.util'
 
-export function wrapHandler<T extends Event>(setter: CallableFunction) {
+export type HasTarget<K> = {
+	target: K
+}
+
+export type InputElement = HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+
+export function wrapHandler<K extends HTMLInputElement, T extends HasTarget<K>>(setter: CallableFunction) {
 	return function eventHandler(event: T) {
-		const target = event.target as HTMLInputElement
-		const { name, type, value, checked } = target
+		const target = event.target as K
+		const { name, type, value } = target
 
 		const path = normalizePath(name)
 
-		setter(...path, type === 'checkbox' ? checked : value)
+		let finalValue: string | boolean = value
+
+		if (type === 'checkbox' && 'checked' in target) {
+			finalValue = target.checked
+		}
+
+		setter(...path, finalValue)
 	}
 }
